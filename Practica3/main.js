34208229216e72ebabd243aa0f914df98b26264c
Maria Eugenia Leyva Avila <122042647@upq.edu.mx>
2025-05-21 20:17:25 -0600

    restar (main)

---
+++ Practica3/main.js
@@ -0,0 +1,9 @@
+const restar = require('./util.js');
+
+const resultado1=restar(1958, 58);
+const resultado2=restar(250, 60);
+const resultado3=restar(15, 2);
+
+console.log(`Resultado 1: ${resultado1}`);
+console.log(`Resultado 2: ${resultado2}`);
+console.log(`Resultado 3: ${resultado3}`);
