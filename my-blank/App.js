6107ae12ff504938687b272644ad1de38ec731f2
Maria Eugenia Leyva Avila <122042647@upq.edu.mx>
2025-06-10 17:13:14 -0600

    Avance práctica 4

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,15 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//Zona 1, IMPORTACIONES
 
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const Texto=(props) => {
+  const {contenido}=props
+  return (
+    <Text>{contenido}</Text>
+  );
+}
+
+
+// Zona 2, MAIN (ejecucuión del programa)
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+
+      <Texto contenido="Hola"></Texto>
+      <Texto contenido="mundo"></Texto>
+      <Texto contenido="React"></Texto>
+      <Texto contenido="Native"></Texto>
+      <Text>Hola Mundo, React Native</Text>
+      <Button title="Tlabaja"></Button>
+       
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
+
+//Zona 3, ESTILOS y CSS (lo bonito)
 const styles = StyleSheet.create({
   container: {
     flex: 1,
