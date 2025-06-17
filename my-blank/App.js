f44ac52a23aa3c972bbedeb6a0dd8fc0109db015
Maria Eugenia Leyva Avila <122042647@upq.edu.mx>
2025-06-17 16:41:55 -0600

    StyleSheet & Flex

--- my-blank/App.js
+++ my-blank/App.js
@@ -2,51 +2,43 @@
 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import React,{useState} from 'react';
+import React, {useState} from 'react';
 
-const Texto=() => {
-  const [contenido,setContenido]=useState("Hola Mundo, React Native");
-  const actualizaTexto=()=>{setContenido("Texto actualizado");}
-  return (
-    <Text onPress={actualizaTexto}>{contenido}</Text>
-  );
-}
+const Texto = ({style}) => {
+  const [contenido, setContenido] = useState('Hola mundo React Native');
+  const actualizaTexto = () => {
+    setContenido('Estado actualizado del texto');
+  };
+  return(<Text style={[styles.text, style]} onPress={actualizaTexto}>{contenido}</Text>)
+};
 
-//cambiar contenido del boton al pulsar
-const Boton=() => {
-  const [textoBoton,setTextoBoton]=useState("Tlabaja");
-  const actualizaBoton=()=>{setTextoBoton("Ya no");}
-  return (
-  <Button title={textoBoton} onPress={actualizaBoton}></Button>
-  );
-}
 
 // Zona 2, MAIN (ejecucuión del programa)
 export default function App() {
   return (
     <View style={styles.container}>
-
-
-      <Texto></Texto>
-      <Texto></Texto>
-      <Texto></Texto>
-      <Texto></Texto>
-      <Text>Hola Mundo, React Native</Text>
-      <Boton></Boton>
-       
+      <Texto style={styles.red}></Texto>
+      <Texto style={styles.blue}></Texto>
+      <Texto style={styles.green}></Texto>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-
-
 //Zona 3, ESTILOS y CSS (lo bonito)
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'yellow',
+    alignItems: 'baseline',
     justifyContent: 'center',
+    flexDirection:'row'
   },
+  text: {
+    color: 'white', 
+    fontsize:27,
+  },
+  red:{backgroundColor:'red'},
+  blue:{backgroundColor:'blue'},
+  green:{backgroundColor:'green'},
 });
