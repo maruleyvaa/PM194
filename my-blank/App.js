d7b5af951ae7fb83bd55539b349168b801d8c476
Maria Eugenia Leyva Avila <122042647@upq.edu.mx>
2025-06-18 19:49:38 -0600

    Buttons

--- my-blank/App.js
+++ my-blank/App.js
@@ -4,20 +4,26 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import React, {useState} from 'react';
 
+
 const Texto = ({style}) => {
   const [contenido, setContenido] = useState('Hola mundo React Native');
   const actualizaTexto = () => {
-    setContenido('Estado actualizado del texto');
+    setContenido('Hola mundo cómo estás?');
   };
-  return(<Text style={[styles.text, style]} onPress={actualizaTexto}>{contenido}</Text>)
+   return (
+    <View Style={{margin:10}}>
+    <Text Style={[styles.text. style]}>{contenido}</Text>
+    <Button title='actualizaTexto'onPress={actualizaTexto}color="purple"/>
+    </View>
+    );
 };
-
 
 // Zona 2, MAIN (ejecucuión del programa)
 export default function App() {
   return (
     <View style={styles.container}>
       <Texto style={styles.red}></Texto>
+    
       <Texto style={styles.blue}></Texto>
       <Texto style={styles.green}></Texto>
       <StatusBar style="auto" />
