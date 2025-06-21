b4b4fafe5d184ffe47547bac8f687b1585592d52
Maria Eugenia Leyva Avila <122042647@upq.edu.mx>
2025-06-20 20:00:06 -0600

    TextInput y Alert

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,81 +1,110 @@
 //Zona 1, IMPORTACIONES
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Switch } from 'react-native';
 import React, {useState} from 'react';
-import { FlatList, SectionList } from 'react-native';
+import { TextInput, Alert, ScrollView } from 'react-native-web';
+
 
 // Zona 2, MAIN (ejecucuión del programa)
-
-const frutas = ['Manzana', 'Banana', 'Pera', 'Sandía'];
-
-const secciones = [
-  {
-    title: 'Frutas',
-    data: ['Naranja', 'Uva', 'Mango'],
-  },
-  {
-    title: 'Verduras',
-    data: ['Zanahoria', 'Lechuga', 'Brócoli'],
-  },
-];
-
 export default function App() {
+  const[nombre,setNombre] = useState('');
+  const[password,setPassword] = useState('');
+  const[comments, setComments] = useState('');
+  const[age, setAge] = useState('');
+
+const showAlert = () => {
+if(nombre.trim() === '' || password.trim() === '' || age.trim() === '') {
+    window.alert('Por favor, completa todos los campos.');
+  }
+  else{
+    window.alert(
+      `Nombre: ${nombre}\nContraseña: ${password}\nEdad: ${age}\nComentarios: ${comments}`,
+      [{ text: 'OK' }],
+    );
+  }
+}
+
   return (
-    <View style={styles.mainContainer}>
-      <StatusBar style="auto" />
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.label}>Nombre</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Escribe tu nombre"
+         value={nombre}
+        onChangeText={setNombre}
+      />
+      <Text style={styles.label}>Contraseña</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Escribe tu contraseña"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry={true}
+      />
+      
+      <Text style={styles.label}>Edad</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Escribe un numero"
+        value={age}
+        onChangeText={setAge}
+        keyboardType="numeric"
+      />
 
-      <View style={styles.section}>
-        <Text style={styles.title}>FlatList</Text>
-        <FlatList
-          data={frutas}
-          keyExtractor={(item, index) => item + index}
-          renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
-        />
-      </View>
+      <Text style={styles.label}>Comentarios Multilineal</Text>
+      <TextInput
+        style={[styles.input, {height:100, textAlignVertical: 'top'}]}
+        placeholder="Escribe tus comentarios"
+        value={comments}
+        onChangeText={setComments}
+        multiline={true}
+        numberOfLines={4}
+      />
 
-      <View style={styles.section}>
-        <Text style={styles.title}>SectionList</Text>
-        <SectionList
-          sections={secciones}
-          keyExtractor={(item, index) => item + index}
-          renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
-          renderSectionHeader={({ section: { title } }) => (
-            <Text style={styles.header}>{title}</Text>
-          )}
-        />
-      </View>
-    </View>
+      <text style={styles.label}>Campo de solo lectura</text>
+      <TextInput
+        style={styles.input}
+        value="Este campo es de solo lectura"
+        editable={false}
+      />
+
+      <button
+        style={{backgroundColor: 'blue', color: 'white', padding: 10, borderRadius: 5}}
+        onClick={showAlert}
+      >
+        Enviar
+      </button>
+
+
+    </ScrollView>
   );
 }
 
-//Zona 3, ESTILOS y CSS (lo bonito)
-const styles = StyleSheet.create({
-  mainContainer: {
-    flex: 1,
-    paddingTop: 20,
-    backgroundColor: 'purple',
-  },
-  section: {
-    marginVertical: 10,
-    paddingHorizontal: 20,
-  },
 
-  title: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    marginVertical: 5,
+// Zona 3, ESTILOS
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    backgroundColor: 'lightpink',
+    padding: 20,
+    alignItems: 'stretch',
+    justifyContent: 'center',
   },
-  item: {
+  label: {
     fontSize: 16,
-    padding: 6,
-    marginLeft: 10,
+    marginTop: 10,
+    color: '#333',
   },
-  header: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    backgroundColor: '#f9c2ff',
-    padding: 5,
+  input: {
+    borderWidth: 1,
+    borderColor: '#999',
+    borderRadius: 5,
+    padding: 10,
+    backgroundColor: '#fff',
+    marginBottom: 10,
+  },
+  textArea: {
+    height: 100,
   },
 });
-
