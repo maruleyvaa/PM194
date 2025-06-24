d5861928d3c23bf4264788355eec19dbc4055bf8
Maria Eugenia Leyva Avila <122042647@upq.edu.mx>
2025-06-24 16:22:04 -0600

    SplashScreen

--- my-blank/App.js
+++ my-blank/App.js
@@ -2,83 +2,25 @@
 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Switch } from 'react-native';
-import React, {useState} from 'react';
+import React, {use, useEffect,useState} from 'react';
 import { TextInput, Alert, ScrollView } from 'react-native-web';
+import * as SplashScreen from 'expo-splash-screen';
 
 
+
+SplashScreen.preventAutoHideAsync(); // Evita que la pantalla de carga se oculte automáticamente
 // Zona 2, MAIN (ejecucuión del programa)
 export default function App() {
-  const[nombre,setNombre] = useState('');
-  const[password,setPassword] = useState('');
-  const[comments, setComments] = useState('');
-  const[age, setAge] = useState('');
+  const[spash,setSplash] = useState(false);
 
-const showAlert = () => {
-if(nombre.trim() === '' || password.trim() === '' || age.trim() === '') {
-    window.alert('Por favor, completa todos los campos.');
-  }
-  else{
-    window.alert(
-      `Nombre: ${nombre}\nContraseña: ${password}\nEdad: ${age}\nComentarios: ${comments}`,
-      [{ text: 'OK' }],
-    );
-  }
-}
-
-  return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.label}>Nombre</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Escribe tu nombre"
-         value={nombre}
-        onChangeText={setNombre}
-      />
-      <Text style={styles.label}>Contraseña</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Escribe tu contraseña"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry={true}
-      />
+  useEffect(() => {
+    setTimeout(async() => {
+      setAppReady(true);
+      await SplashScreen.hideAsync(); // Oculta la pantalla de carga después de 2 segundos
+    },2000);
+   }, []);
+  
       
-      <Text style={styles.label}>Edad</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Escribe un numero"
-        value={age}
-        onChangeText={setAge}
-        keyboardType="numeric"
-      />
-
-      <Text style={styles.label}>Comentarios Multilineal</Text>
-      <TextInput
-        style={[styles.input, {height:100, textAlignVertical: 'top'}]}
-        placeholder="Escribe tus comentarios"
-        value={comments}
-        onChangeText={setComments}
-        multiline={true}
-        numberOfLines={4}
-      />
-
-      <text style={styles.label}>Campo de solo lectura</text>
-      <TextInput
-        style={styles.input}
-        value="Este campo es de solo lectura"
-        editable={false}
-      />
-
-      <button
-        style={{backgroundColor: 'blue', color: 'white', padding: 10, borderRadius: 5}}
-        onClick={showAlert}
-      >
-        Enviar
-      </button>
-
-
-    </ScrollView>
-  );
 }
 
 
