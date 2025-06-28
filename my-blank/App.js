8eca4c2e2c83a62fc06c1b8fc7ecc62308bd3f41
Maria Eugenia Leyva Avila <122042647@upq.edu.mx>
2025-06-27 20:07:30 -0600

    Activity Indicator

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,167 +1,66 @@
 //Zona 1, IMPORTACIONES
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground, ScrollView} from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, ScrollView, ActivityIndicator} from 'react-native';
 import React, { useEffect, useState } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
+import { Button } from 'react-native-web';
 
 //Zona 2
 export default function App() {
-  return(
-    <ScrollView contentContainerStyle={styles.background} showsVerticalScrollIndicator={false} horizontal={true}>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      <text>Hola mundo</text>
-      
 
-      
-      
-    </ScrollView>
-  )
+  const [loading, setLoading] = useState(false);
+  const [mensaje, setMensaje] = useState('');
 
+  const simularcarga = () => {
+    setLoading(true);
+    setMensaje('');
+    setTimeout(() => {
+      setLoading(false);
+      setMensaje('Carga completada');
+    }, 3000);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        carga
+      </Text>
+      {loading ? (
+        <View>
+          <ActivityIndicator size="large" color="#2D9CDB"/>
+          <Text style={styles.subtitle}> CARGANDO... </Text>
+        </View>
+      ) : (
+        <>
+        <Button title ="Iniciar Carga" onPress={simularcarga} />
+        {mensaje !== ''&& <Text style={styles.exito}>{mensaje}</Text>}
+        </>
+      )}
+    </View>
+  );
 }
-
+    
 
 
 // Zona 3, ESTILOS
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    width: "100%",
-    height: "100%",
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   container: {
     flex: 1,
-    padding: 20,
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
   title: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    color: '#fff'
+    fontSize: 22,
+    marginBottom: 20
   },
-  subtitle: {
-    fontSize: 16,
-    color: '#fff'
+  texto:{
+    marginTop:15,
+    color: 'gray',
+  },
+  exito: {
+    marginTop: 20,
+    color: 'green',
+    fontWeight: 'bold',
   }
 });
